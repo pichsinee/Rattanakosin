9c5d11dd533e8b13a0fa3ef7e01178b3c74480b3
Pichsinee <p.pichsinee@gmail.com>
2017-01-07 14:15:38 +0700

    bind Finish

--- app/src/main/java/rattanakosin/alice/rattanakosin/MainActivity.java
+++ app/src/main/java/rattanakosin/alice/rattanakosin/MainActivity.java
@@ -2,15 +2,25 @@ package rattanakosin.alice.rattanakosin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     // Explicit ประกาศตัวแปร
-
+    private Button signInButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+
+
     } // Main Method
+
+    // คือการผูกความสัมพันธ์ระหว่าง ตัวแปร และ ออปเจ็ค บน Activity
+    private void bindWidget() {
+
+    }
 } // Main Class
