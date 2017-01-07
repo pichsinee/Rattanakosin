e81e6bddf06179122453623aa995c79cbefea14d
Pichsinee <p.pichsinee@gmail.com>
2017-01-07 15:00:17 +0700

    Finish Day1

--- app/src/main/java/rattanakosin/alice/rattanakosin/MainActivity.java
+++ app/src/main/java/rattanakosin/alice/rattanakosin/MainActivity.java
@@ -1,7 +1,9 @@
 package rattanakosin.alice.rattanakosin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,12 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bindWidget();
-
+        buttonController();
 
     } // Main Method
 
+    private void buttonController() {
+        // For signUp
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+    }// buttonController
+
     // คือการผูกความสัมพันธ์ระหว่าง ตัวแปร และ ออปเจ็ค บน Activity
     private void bindWidget() {
+
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);  //ใช้คำสั่ง cast to โดยกด Alt+Enter
 
     }
 } // Main Class
